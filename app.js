6067ca38b5b8f8bf24517f7dab64e04edfe30487
DavidTMaciel <davidmacieljr122@gmail.com>
2023-05-30 19:58:48 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const  PORT = process.env.PORT || 8081;
+const app = express();
+
+mongoose.connect(
+    process.env.MONGO_URI || "mongodb://0.0.0.0:27017/CRUD", 
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }).then( () => {
+        console.log("Connected to MongoDB");
+        
+    }).catch(function(erro){
+        console.log(erro);
+    }); 
+
+
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log('Server is running on port :' + PORT);
+})
